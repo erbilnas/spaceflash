ee3280947e47f49d476da63eb6aa47be10524edd
Erbil Nas <erbil.nas@trendyol.com>
2022-01-08 17:36:59 +0300

    add laserbeams

---
+++ Assets/Scripts/LaserBeamMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBeamMove : MonoBehaviour
+{
+	public float thrust = 10f;
+	private Rigidbody rb;
+	// Start is called before the first frame update
+	private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = transform.forward * thrust;
+
+        Destroy(gameObject, 2f);
+    }
+}
