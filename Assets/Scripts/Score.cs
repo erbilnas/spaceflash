94712d9d8ee3014b87060fa48ba7ab51e107fe9a
Erbil Nas <erbil.nas@trendyol.com>
2022-01-11 20:26:15 +0300

    add game manager and some UI elements

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+    private void OnEnable() {
+        GameManager.OnAsteroidDestroyed += DisplayScore;
+    }
+
+    private void OnDisable() {
+        GameManager.OnAsteroidDestroyed -= DisplayScore;
+    }
+
+    private void DisplayScore(){
+        GetComponent<TextMeshProUGUI>().text = "Score: " + GameManager.playerScore.ToString();
+    }
+}
---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButton : MonoBehaviour
+{
+    public GameManager gameManager;
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Laser")) {
+            gameManager.StartGame();
+        }
+    }
+}
